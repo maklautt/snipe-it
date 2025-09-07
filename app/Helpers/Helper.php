685bbf4375d305ef5824e243fd841ff9320c8045
snipe <snipe@snipe.net>
2025-09-07 08:35:23 +0100

    Ugh, this is awful
    Signed-off-by: snipe <snipe@snipe.net>

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -82,6 +82,13 @@ class Helper
         'zu' => 'zu-ZA', // Zulu
     ];
 
+    public static function hasRtl($value) {
+        $rtlChar = '/[\x{0590}-\x{083F}]|[\x{08A0}-\x{08FF}]|[\x{FB1D}-\x{FDFF}]|[\x{FE70}-\x{FEFF}]/u';
+        return preg_match($rtlChar, $value) != 0;
+    }
+
+
+
     /**
      * Simple helper to invoke the markdown parser
      *
--- resources/views/account/accept/accept-asset-eula.blade.php
+++ resources/views/account/accept/accept-asset-eula.blade.php
@@ -1,9 +1,11 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}"
+      dir="{{ Helper::determineLanguageDirection() }}">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <style>
         body {
             font-family:'Dejavu Sans', Arial, Helvetica, sans-serif;
@@ -31,7 +33,12 @@
 
 @if ($eula)
     <hr>
-    {!!  $eula !!}
+
+    {!! $eula !!}
+
+    {!!  str_replace('<p>', '<p dir="auto">', $eula) !!}
+
+     {{ str_replace('<p>', '<p dir="auto">', $eula) }}
     <hr>
 @endif
 
